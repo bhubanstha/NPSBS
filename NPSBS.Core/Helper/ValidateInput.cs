11dd99abe9ac370afc29bb3d2725679f8268e6ee
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2020-03-05 15:33:29 +0545

    file renamed for validation

---
+++ NPSBS.Core/Helper/ValidateInput.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace NPSBS.Core
+{
+	public class ValidateInput
+	{
+		public static void Yes(KryptonTextBox textbox, object sender, KeyPressEventArgs e)
+		{
+			if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+			{
+				e.Handled = true;
+			}
+
+		}
+
+		public static void DecimalType(KryptonTextBox textbox, object sender, KeyPressEventArgs e)
+		{
+
+			if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+			{
+				e.Handled = true;
+			}
+
+			// only allow one decimal point
+			if ((e.KeyChar == '.') && ((sender as KryptonTextBox).Text.IndexOf('.') > -1))
+			{
+				e.Handled = true;
+			}
+			if (!char.IsControl(e.KeyChar))
+			{
+
+				KryptonTextBox textBox = (KryptonTextBox)sender;
+
+				if (textBox.Text.IndexOf('.') > -1 &&
+								 textBox.Text.Substring(textBox.Text.IndexOf('.')).Length >= 3)
+				{
+					e.Handled = true;
+				}
+
+			}
+		}
+
+		public static void StringOnly(KryptonTextBox textbox, object sender, KeyPressEventArgs e)
+		{
+			e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Space);
+		}
+
+		public static void StringOnly(KryptonTextBox textbox, object sender, KeyPressEventArgs e, bool allowPlusSymbol)
+		{
+			e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Space || e.KeyChar == (char)Keys.Execute);
+		}
+	}
+}
