2a11fcc4d8da161bb20777cdcb44c67fbfca6c90
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2020-03-13 20:31:23 +0545

    montessori db migrations merged

--- DbMigration/dbUpdate-Montessori.sql
+++ DbMigration/dbUpdate-Montessori.sql
@@ -206,3 +206,118 @@ begin
 	where sc.EnrolledYear = (select ISNULL(MAX(EnrolledYear),0) from StudentClass)
 end
 
+GO
+
+ALTER proc [dbo].[usp_Student_Select_By_Class_Year]
+@ClassId int,
+@EnrolledYear varchar(4),
+@SubjectId int,
+@ExaminationId int
+as
+begin
+	select s.StudentId, sc.RollNumber as [Roll Number], s.StudentFullName as [Student Name], se.Grade 
+	 from Student s
+	inner join StudentClass sc on s.StudentId = sc.StudentId
+	LEFT JOIN StudentExams se on s.StudentId = se.StudentId and sc.ClassId = sc.ClassId 
+	AND se.SubjectId = @SubjectId
+	AND se.ExaminationId = @ExaminationId
+	 LEFT JOIN Subjects sub on se.SubjectId = sub.SubjectId
+	where sc.ClassId = @ClassId AND sc.EnrolledYear = @EnrolledYear
+	ORDER BY convert(int, sc.RollNumber) ASC
+end
+
+GO
+
+ALTER proc [dbo].[usp_StudentExam_Save]
+@RollNumber varchar(5),
+@ExaminationId int,
+@ClassId int,
+@SubjectId int,
+@Grade nchar(2),
+@ExamYear nvarchar(4),
+@StudentId int
+as
+begin
+begin try
+begin tran
+	
+	if EXISTS(SELECT 'a' FROM StudentExams WHERE StudentId = @StudentId AND ExaminationId = @ExaminationId AND ClassId = @ClassId AND SubjectId = @SubjectId)
+	BEGIN
+		UPDATE StudentExams SET Grade = @Grade
+			 WHERE StudentId = @StudentId AND ExaminationId = @ExaminationId AND ClassId = @ClassId AND SubjectId = @SubjectId
+	END
+	ELSE
+	BEGIN
+		INSERT INTO StudentExams (StudentId, ExaminationId, ClassId, SubjectId, Grade) VALUES
+		(@StudentId, @ExaminationId, @ClassId, @SubjectId, @Grade)
+	END
+commit
+end try
+begin catch
+	rollback
+end catch
+end
+GO
+	
+ALTER proc [dbo].[usp_ExamResult]
+@ExaminationId int,
+@ClassId int,
+@Year varchar(5)
+as
+begin
+	declare @minId int, @maxId int
+	
+	select SN=identity(int ,1,1), sub.ClassId, s.StudentId, ex.ExamName, e.ExamHeldYear, e.ResultPrintDate, 
+	c.ClassName, s.StudentFullName,  sc.RollNumber, sub.SubjectName,  se.Grade as 'ObtainedGrade', r.Remarks
+	INTO #tempResult   FROM StudentExams se
+	INNER JOIN Student s on se.StudentId = s.StudentId
+	INNER JOIN Examination e on se.ExaminationId = e.ExaminationId
+	INNER JOIN Exam ex on e.ExamId = ex.ExamID
+	INNER JOIN Class c on se.ClassId = c.ClassId
+	INNER JOIN StudentClass sc on se.StudentId = sc.StudentId AND se.ClassId = sc.ClassId
+	INNER JOIN Subjects sub on se.SubjectId = sub.SubjectId
+	LEFT JOIN Remarks r ON s.StudentId  =r.StudentId AND sc.ClassId = r.ClassId AND e.ExaminationId = r.ExaminationId
+	WHERE se.ExaminationId = @ExaminationId AND se.ClassId = @ClassId and  e.ExamHeldYear = @Year
+	
+	SELECT DISTINCT  StudentId into #StudentRecord FROM #tempResult
+	
+	
+	set @minId = (SELECT MIN(StudentId) from #StudentRecord)
+	set @maxId = (SELECT MAX(StudentId) from #StudentRecord)
+	WHILE (@minId<= @maxId)
+	BEGIN
+		if(SELECT COUNT(*) from	#tempResult WHERE StudentId = (SELECT StudentId FROM #StudentRecord WHERE StudentId= @minId) )>0
+	BEGIN	
+		SELECT * FROM #tempResult WHERE StudentId = (SELECT StudentId FROM #StudentRecord WHERE StudentId= @minId) ORDER BY ClassID
+		END
+		SET @minId = @minId +1
+	END
+	
+	DROP TABLE #tempResult
+	DROP TABLE #StudentRecord
+end
+GO
+
+
+ALTER proc [dbo].[usp_Remarks_Save]
+	@StudentId int,
+	@ClassId int,
+	@ExaminationId int,
+	@ExamHeldYear int,
+	@Remark nvarchar (250),
+	@RemarkId int
+as
+begin
+	if (@RemarkId=0)
+	BEGIN
+		INSERT INTO Remarks(StudentId, ClassId, ExaminationId, ExamHeldYear, Remarks) VALUES
+			(@StudentId, @ClassId, @ExaminationId, @ExamHeldYear, @Remark)
+	END
+	ELSE
+	BEGIN
+		UPDATE Remarks SET Remarks =@Remark WHERE RemarksId = @RemarkId
+	END
+end
+
+
+GO
